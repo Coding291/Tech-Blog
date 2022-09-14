1fd270aaeda53bd6d6c628bd1ca00f73890ce0fb
Muhammad Humayun <humayun@MacBook-Pro.home>
2022-09-14 14:40:01 -0400

    fixed errors

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Session expires in 15 minutes
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        expiration: 15*60*1000
+    })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {}, 
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// Static pages
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Use session in app to track user data
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+//  API routes
+app.use(routes);
 
+// Handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(require('./controllers/'));
-
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// turn on connection to db and routes
+sequelize.sync({ alter: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
