51dcb1776a9fcfb282c7e011aa637255c944d52b
Muhammad Humayun <humayun@MacBook-Pro.home>
2022-09-14 14:55:23 -0400

    fixed some errors

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,14 +1,19 @@
 async function logout() {
-    const response = await fetch('/api/users/logout', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+  try {
+      const response = await fetch('/api/users/logout', {
+          method: 'post',
+          headers: { 'Content-Type': 'application/json' },
+      });
+
+      // check the response status
+      if (response.ok) {
+          document.location.replace('/');
+      } else {
+          alert(response.statusText);
+      }
+  } catch (error) {
+      alert(error);
   }
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,27 +1,35 @@
+// sign up/register new account
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/login');
-      } else {
-        alert(`You have entered an invalid input. Password must be at least 4 characters long.`);
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+      try {
+          const response = await fetch('/api/users', {
+              method: 'post',
+              body: JSON.stringify({
+                  username,
+                  email,
+                  password,
+              }),
+              headers: { 'Content-Type': 'application/json' },
+          });
+
+          // check the response status
+          if (response.ok) {
+              document.location.replace('/dashboard');
+          } else {
+              alert(response.statusText);
+          }
+      } catch (error) {
+          alert(error);
       }
-    }
   }
-  
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+}
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
